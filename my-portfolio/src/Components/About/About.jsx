2da0258059680a4bfc2d2798b64f735e44270b04
maze6343 <maze6343@student.su.se>
2024-05-06 10:23:18 +0200

    apdate about sections

--- my-portfolio/src/Components/About/About.jsx
+++ my-portfolio/src/Components/About/About.jsx
@@ -1,47 +1,52 @@
 import React from "react";
 import "./About.css";
-import img from '../../assets/profile-img.jpg';
+import img from "../../assets/profile-img.jpg";
 
 const About = () => {
   return (
-    <div id='About' className="about">
-        <div className="about-title">
-            <h1>About me</h1>
-        </div>
+    <div id="About" className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+      </div>
       <div className="about-sections">
         <div className="about-left">
-        <div className="about-para">
+          <div className="about-para">
+            <h1>About Me</h1>
             <p>
-              Som en datavetenskapsstudent som examineras 2024 från Stockholms
-              universitet, har jag uppnått kompetens inom programvaruutveckling,
-              databaser, algoritmer och webbutveckling. Min utbildning har
-              finslipat mina färdigheter och passion för att skapa innovativa
-              lösningar. Jag ser fram emot att samarbeta och bidra till
-              banbrytande projekt.
+              As a computer science student graduating in 2024 from Stockholm
+              University, I have acquired skills in software development,
+              databases, algorithms, and web development. My education has honed
+              my skills and passion for creating innovative solutions. I am
+              looking forward to collaborating and contributing to
+              groundbreaking projects.
             </p>
           </div>
         </div>
         <div className="about-right">
-          
           <div className="about-skills">
             <div className="about-skill">
+              <h1>Education</h1> <br></br>
               <p>
-                HTML & CSS <hr/>
+              STOCKHOLM UNIVERSITY Bachelor in Computer Science 2020-2024
+                <hr />
               </p>
             </div>
             <div className="about-skill">
+              <h1>Technical Skills</h1>
+              <p>Web Development: HTML, CSS, JavaScript.</p>
+              <p>Languages: Java, C++, C#, JavaScript, PHP.</p>
+              <p>Database Management: SQL, NoSQL.</p>
+              <p>Algorithm Design and Analysis.</p>
+              <p>Software Development.</p>
               <p>
-                React JS <hr style={{ width: "60%" }} />
+                Human-Computer Interaction (HCI).
+                <hr style={{ width: "60%" }} />{" "}
               </p>
             </div>
             <div className="about-skill">
+              <h1>Languages</h1>
               <p>
-                JavaScript <hr style={{ width: "70%" }} />
-              </p>
-            </div>
-            <div className="about-skill">
-              <p>
-                Next JS <hr style={{ width: "50%" }} />
+                English, Swedish <hr style={{ width: "50%" }} />
               </p>
             </div>
           </div>
