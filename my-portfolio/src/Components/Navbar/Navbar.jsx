3f8470e4b8f5911bc137cffe70a91c28b0fba76a
maze6343 <maze6343@student.su.se>
2024-05-11 21:26:57 +0200

    build app

--- my-portfolio/src/Components/Navbar/Navbar.jsx
+++ my-portfolio/src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   };
   return (
     <div className="navbar">
-      <h1>Portfolio</h1>
+      <h2>Marah Zeibak</h2>
       <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open" />
       <ul ref={menuRef} className="nav-menu">
         <img
