b5f24c40fa3453397fa8aafdbaa238ff8f43f006
maze6343 <maze6343@student.su.se>
2024-05-15 20:45:29 +0200

    change hero image

--- my-portfolio/src/Components/Header/Header.jsx
+++ my-portfolio/src/Components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import MovingComponent from "react-moving-text";
 import "./Header.css";
-import backgrund from "../../assets/backgrund.jpg";
+import backgrund from "../../assets/backgrund.jpeg";
+import small from "../../assets/backgrundSmall.jpeg";
 
 const Header = () => {
   return (
     <div id="Header" className="header">
       <div className="background-img">
-        <img src={backgrund} alt="hero image" />
+        <img src={backgrund} alt="hero image"className="imageBig"/>
+        <img src={small} alt="hero image"  className="imageSmall"/>
       </div>
       <h1>
         Hi, I'm{" "}
